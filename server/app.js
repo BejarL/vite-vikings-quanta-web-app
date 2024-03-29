a1db4a077c71a9ae574fe22642f7c7d1d24c95ac
BenjaminMaring <benjammaring@gmail.com>
2024-03-29 17:37:40 -0600

    fixed connection error, added endpoint to create users

--- server/app.js
+++ server/app.js
@@ -5,9 +5,6 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const dbConnect = require('./database');
 
-// Allows us to access the .env
-require('dotenv').config();
-
 const app = express();
 const port = 3000;
 
@@ -26,15 +23,56 @@ app.use(bodyParser.json());
 //adds database connection to req.db 
 app.use((req, res, next) => dbConnect(req, res, next));
  
-app.post('/test', async (req, res) => {
-    console.log("hit")
- 
-    if (req.db) {
-        console.log("connected")
-    }
+app.put('/signup', async (req, res) => {
+    try {
+        //get the data from the request to create a new user
+        const {
+            username,
+            email,
+            password
+        } = req.body;
 
-    res.send("working");
-})
+        //check if the email or username is being used
+        const [[validateEmail]] = await req.db.query(`SELECT email FROM Users WHERE email = :email`, { email });
+        const [[validateUser]] = await req.db.query(`SELECT username FROM Users WHERE username = :username`, { username });
+
+        if (validateEmail) {
+            res.json({success: false, err: "Email already in use"});
+        } else if (validateUser) {
+            res.json({success: false, err: "Username already taken"});
+        } else {
+
+            //hash their password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            console.log(hashedPassword);
+            
+            //attempt to insert the data into the database
+            await req.db.query(`INSERT INTO Users (username, email, password) 
+                                                          VALUES (:username, :email, :hashedPassword);`, 
+            { username, email, hashedPassword });
+
+            //need to query into the db to get the newly created users id, username, and profilepic
+            const [[userData]] = await req.db.query(`SELECT user_id, username, email FROM Users WHERE username = :username`, {
+                username
+            });
+
+            const payload = {
+                user_id: userData.user_id,
+                username: userData.username,
+                email: userData.email
+            }
+
+            const jwtEncodedUser = jwt.sign(payload, process.env.JWT_KEY);
+                
+            res.json({ jwt: jwtEncodedUser, success: true, data: payload });
+        }
+    } catch(error) {
+        res.json({success: false, err: error});
+        console.log(`error creating user ${error}`);
+    }
+}) // end user post
+
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
--- server/database.js
+++ server/database.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql2/promise');
 
+//Allows us to access the ENV file
+require('dotenv').config();
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
