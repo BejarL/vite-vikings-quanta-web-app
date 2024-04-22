9066dd3f87a9f4e9aa41441a6fd01bf375681988
Bejar L <143468066+BejarL@users.noreply.github.com>
2024-04-22 14:53:42 -0700

    New project and workspace modals

--- client/src/pages/ConfirmResetPasswordPage.jsx
+++ client/src/pages/ConfirmResetPasswordPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { verifyData } from "../Auth/jwt.js"
 
 const ConfirmResetPasswordPage = () => {
   const [newPassword, setNewPassword] = useState("");
@@ -12,11 +11,11 @@ const ConfirmResetPasswordPage = () => {
 
   const handlePasswordUpdate = (e) => {
     setNewPassword(e.target.value);
-  }
+  };
 
   const handleConfirmUpdate = (e) => {
     setConfirm(e.target.value);
-  }
+  };
 
   //api call to reset password
   const resetPassword = async () => {
@@ -30,10 +29,10 @@ const ConfirmResetPasswordPage = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          authorization: `Bearer ${jwt}`
+          authorization: `Bearer ${jwt}`,
         },
         body: JSON.stringify({
-          password: newPassword
+          password: newPassword,
         }),
       });
 
@@ -42,77 +41,76 @@ const ConfirmResetPasswordPage = () => {
 
       //if the password was reset, navigate to login page
       if (success) {
-        navigate("/")
+        navigate("/");
       } else {
         window.alert("Error reseting password");
       }
-
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="bg-lightpurple-login w-[100vw] h-[100vh] md:p-[50px]">
-    <div className="z-[1] h-[100%] flex items-center justify-center md:w-[100%]">
-      <div className="relative bg-white p-9 rounded-lg shadow-md w-[400px] min-w-md min-w-[350px] pt-1 h-[100%] md:flex md:flex-col md:items-center h-[90%] md:w-[70%]">
-        <div className="self-center  text-gray-700 font-bold text-4xl mt-10 flex justify-evenly md:flex md:w-[80%] ">
-          <p>Reset password</p>
+      <div className="z-[1] h-[100%] flex items-center justify-center md:w-[100%]">
+        <div className="relative bg-white p-9 rounded-lg shadow-md w-[400px] min-w-md min-w-[350px] pt-1 md:flex md:flex-col md:items-center h-[90%] md:w-[70%]">
+          <div className="self-center  text-gray-700 font-bold text-4xl mt-10 flex justify-evenly md:flex md:w-[80%] ">
+            <p>Reset password</p>
+          </div>
+          <div className="self-center mt-4 flex mb-4 justify-evenly md:flex md:w-[80%] md:mb-[100px]">
+            <p>
+              Please enter the email address you would like your password reset
+              link sent to
+            </p>
+          </div>
+          <form action="" className="w-full md:w-[90%]">
+            <div className="mb-4 md:w-[100%] ">
+              <label
+                htmlFor="new-password"
+                className="block text-gray-700 text-sm font-bold mb-2 "
+              >
+                New Password
+              </label>
+              <input
+                type="text"
+                placeholder="Enter new password"
+                className="appearance-none w-full bg-transparent border-0 border-b border-gray-700 focus:outline-none py-1 md:mb-[20px]"
+                id="new-password"
+                value={newPassword}
+                onChange={handlePasswordUpdate}
+              />
+            </div>
+            <div className="mb-4 md:w-[100%] ">
+              <label
+                htmlFor="confirm-password"
+                className="block text-gray-700 text-sm font-bold mb-2 "
+              >
+                Confirm Password
+              </label>
+              <input
+                type="text"
+                placeholder="Enter password"
+                className="appearance-none w-full bg-transparent border-0 border-b border-gray-700 focus:outline-none py-1 md:mb-[20px]"
+                id="confirm-password"
+                value={confirm}
+                onChange={handleConfirmUpdate}
+              />
+            </div>
+
+            <div>
+              <button
+                className="text-gray-700 w-full hover:bg-lightpurple bg-lightpurple-login rounded-3xl font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={resetPassword}
+              >
+                Reset password
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="self-center mt-4 flex mb-4 justify-evenly md:flex md:w-[80%] md:mb-[100px]">
-          <p>
-            Please enter the email address you would like your password reset
-            link sent to
-          </p>
-        </div>
-        <form action="" className="w-full md:w-[90%]">
-          <div className="mb-4 md:w-[100%] ">
-            <label
-              htmlFor="new-password"
-              className="block text-gray-700 text-sm font-bold mb-2 "
-            >
-              New Password
-            </label>
-            <input
-              type="text"
-              placeholder="Enter new password"
-              className="appearance-none w-full bg-transparent border-0 border-b border-gray-700 focus:outline-none py-1 md:mb-[20px]"
-              id="new-password"
-              value={newPassword}
-              onChange={handlePasswordUpdate}
-            />
-          </div>
-          <div className="mb-4 md:w-[100%] ">
-            <label
-              htmlFor="confirm-password"
-              className="block text-gray-700 text-sm font-bold mb-2 "
-            >
-              Confirm Password
-            </label>
-            <input
-              type="text"
-              placeholder="Enter password"
-              className="appearance-none w-full bg-transparent border-0 border-b border-gray-700 focus:outline-none py-1 md:mb-[20px]"
-              id="confirm-password"
-              value={confirm}
-              onChange={handleConfirmUpdate}
-            />
-          </div>
-          
-          <div>
-            <button
-              className="text-gray-700 w-full hover:bg-lightpurple bg-lightpurple-login rounded-3xl font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={resetPassword}
-            >
-             Reset password
-            </button>
-          </div>
-        </form>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ConfirmResetPasswordPage
+export default ConfirmResetPasswordPage;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,91 +1,123 @@
-import {useState , useEffect, useContext} from  'react';
-import { workspaceContext } from './Layout';
-import { useNavigate } from 'react-router-dom'
-import { getJwt, verifyData } from '../Auth/jwt';
+import { useState, useEffect, useContext } from "react";
+import { workspaceContext } from "./Layout";
+import { useNavigate } from "react-router-dom";
+import { getJwt, verifyData } from "../Auth/jwt";
+import CreateModal from "../modals/CreateModal";
 
 const HomePage = () => {
-    const [recentProjects, setRecentProjects] = useState([]);
-    const workspace = useContext(workspaceContext);
+  const [recentProjects, setRecentProjects] = useState([]);
+  const workspace = useContext(workspaceContext);
+  const [isModalOpen, setModalOpen] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() =>{
-        handleGetRecent();
-    }, []);
+  useEffect(() => {
+    handleGetRecent();
+  }, []);
 
-    //fetches the recent projects
-    const handleGetRecent = async () => {
-        const jwt = getJwt();
+  //fetches the recent projects
+  const handleGetRecent = async () => {
+    const jwt = getJwt();
 
-        try {
-            const res = await fetch("http://localhost:3000/projects/recent", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "authorization": jwt
-                }, 
-                body: JSON.stringify({
-                    "workspace_id": workspace
-                })
-            })
+    try {
+      const res = await fetch("http://localhost:3000/projects/recent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: jwt,
+        },
+        body: JSON.stringify({
+          workspace_id: workspace,
+        }),
+      });
 
-            // checks only for jwt errors. if there are errors, navigate to sign in
-            // if no error, gets data
-            const { success, data } = await verifyData(res, navigate);
+      // checks only for jwt errors. if there are errors, navigate to sign in
+      // if no error, gets data
+      const { success, data } = await verifyData(res, navigate);
 
-            console.log(data);
-    
-            if (success) {
-                setRecentProjects(data);
-            }
+      console.log(data);
 
-        } catch (err) {
-            console.log(err);
-        }
+      if (success) {
+        setRecentProjects(data);
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const recentElems = recentProjects.map(project => {
-        return (
-            <button key={project.project_id} 
-                className="flex flex-col items-center justify-center m-[10px] max-w-sm min-w-[200px] min-h-[150px] bg-white border border-white-200 rounded-lg shadow hover:bg-white-100 dark:bg-white-800 dark:border-white-700 dark:hover:bg-violet-100">
-                    <h5 className="mb-2  font-bold text-black-900 dark:text-black">
-                        
-                    </h5>
-                    <p className="font-bold text-white-700 dark:text-white-400">{project.project_name}</p>
-                </button> 
-        )
-    })
-
+  const recentElems = recentProjects.map((project) => {
     return (
-    <div>
-        <div className=" flex flex-wrap justify-evenly m-[50px] md:justify-start">
-            <div className="m-[10px]">
-                <button className="flex flex-col items-center justify-center max-w-sm min-w-[200px] min-h-[150px] bg-white border border-white-200 rounded-lg shadow hover:bg-white-100 dark:bg-white-800 dark:border-white-700 dark:hover:bg-violet-100">
-                    <h5 className="mb-2  font-bold text-black-900 dark:text-black">
-                        <svg className="svg2" xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 1200 1200"><path fill="black" d="M600 0C268.629 0 0 268.629 0 600s268.629 600 600 600s600-268.629 600-600S931.371 0 600 0m-95.801 261.841h191.602v242.358h242.358v191.602H695.801v242.358H504.199V695.801H261.841V504.199h242.358z"/>
-                        </svg>
-                    </h5>
-                <p className="font-bold text-white-700 dark:text-white-400">create workspace</p>
-                </button> 
-            </div>
-            <div className="m-[10px]">    
-                <button className="flex flex-col items-center justify-center min-w-[200px] min-h-[150px] max-w-sm bg-white border border-white-200 rounded-lg shadow hover:bg-white-100 dark:bg-white-800 dark:border-white-700 dark:hover:bg-violet-100">
-                    <h5 className=" mb-2  font-bold  text-black-900 dark:text-black">
-                        <svg className="svg2"xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 64 64"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32c0 16.568 13.432 30 30 30s30-13.432 30-30C62 15.432 48.568 2 32 2m1.693 46V37.428H15V27.143h18.693V16L49 32z"/>
-                        </svg>
-                    </h5>
-                <p className="font-bold text-white-700 dark:text-white-400">Join a workspace</p>
-                </button> 
-            </div>
-        </div>
-    <h1 className="font-bold text-3xl md:text-4xl m-[20px]">Recent Projects</h1>
-    <div className="flex flex-wrap justify-center m-[50px] md:justify-start">
-        {recentElems}
-    </div>
-    
+      <button
+        key={project.project_id}
+        className="flex flex-col items-center justify-center m-[10px] max-w-sm min-w-[200px] min-h-[150px] bg-white border border-white-200 rounded-lg shadow hover:bg-white-100 dark:bg-white-800 dark:border-white-700 dark:hover:bg-violet-100"
+      >
+        <h5 className="mb-2  font-bold text-black-900 dark:text-black"></h5>
+        <p className="font-bold text-white-700 dark:text-white-400">
+          {project.project_name}
+        </p>
+      </button>
+    );
+  });
 
-</div>
-    )
-}
+  return (
+    <div>
+      <div className=" flex flex-wrap justify-evenly m-[50px] md:justify-start">
+        <div className="m-[10px]">
+          <button
+            onClick={() => setModalOpen(true)}
+            className="flex flex-col items-center font-bold text-white-700 dark:text-white-400 justify-center max-w-sm min-w-[200px] min-h-[150px] bg-white border border-white-200 rounded-lg shadow hover:bg-white-100 dark:bg-white-800 dark:border-white-700 dark:hover:bg-violet-100"
+          >
+            <h5 className="mb-2  font-bold text-black-900 dark:text-black">
+              <svg
+                className="svg2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="3em"
+                height="3em"
+                viewBox="0 0 1200 1200"
+              >
+                <path
+                  fill="black"
+                  d="M600 0C268.629 0 0 268.629 0 600s268.629 600 600 600s600-268.629 600-600S931.371 0 600 0m-95.801 261.841h191.602v242.358h242.358v191.602H695.801v242.358H504.199V695.801H261.841V504.199h242.358z"
+                />
+              </svg>
+            </h5>
+            Create workspace
+          </button>
+          <CreateModal
+            isOpen={isModalOpen}
+            onClose={() => setModalOpen(false)}
+          />
+        </div>
+        <div className="m-[10px]">
+          <button className="flex flex-col items-center justify-center min-w-[200px] min-h-[150px] max-w-sm bg-white border border-white-200 rounded-lg shadow hover:bg-white-100 dark:bg-white-800 dark:border-white-700 dark:hover:bg-violet-100">
+            <h5 className=" mb-2  font-bold  text-black-900 dark:text-black">
+              <svg
+                className="svg2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="3em"
+                height="3em"
+                viewBox="0 0 64 64"
+              >
+                <path
+                  fill="currentColor"
+                  d="M32 2C15.432 2 2 15.432 2 32c0 16.568 13.432 30 30 30s30-13.432 30-30C62 15.432 48.568 2 32 2m1.693 46V37.428H15V27.143h18.693V16L49 32z"
+                />
+              </svg>
+            </h5>
+            <p className="font-bold text-white-700 dark:text-white-400">
+              Join a workspace
+            </p>
+          </button>
+        </div>
+      </div>
+      <h1 className="font-bold text-3xl md:text-4xl m-[20px]">
+        Recent Projects
+      </h1>
+      <div className="flex flex-wrap justify-center m-[50px] md:justify-start">
+        {recentElems}
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
